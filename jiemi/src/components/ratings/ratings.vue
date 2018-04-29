8f5c210c301e4dadef6ed10b23cc6620073e504e
dongminglugit@hotmail.com <dongminglugit@hotmail.com>
2018-04-29 14:23:37 +0800

    commit

---
+++ jiemi/src/components/ratings/ratings.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="sta">
+      哈哈哈哈哈哈
+    </div>
+</template>
+<script type="text/ecmascript-6">
+  export default {};
+</script>
+<style lang="stylus" rel=stylesheet/stylus>
+</style>
